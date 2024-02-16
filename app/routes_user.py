d7739974505c578c6086b9cf83395dcbe6505e2b
Karl <karl@ltek.me>
2024-02-16 17:03:08 +0800

    half user
    sync local to github

--- app/routes_user.py
+++ app/routes_user.py
@@ -91,3 +91,12 @@ def register():
     # default action
     return render_template('register.html', title='Register', form=form)
 
+
+@flask_app.route('/u/<username>')
+def user(username):
+    user = User.query.filter_by(username = username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)

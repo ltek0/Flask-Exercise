508c7d94337a01d615c20d401fe3bcc8aacbb55c
Karl <karl@ltek.me>
2024-03-18 01:26:09 +0000

    oops

--- db_migtions.py
+++ db_migtions.py
@@ -3,7 +3,7 @@ from os import path as op
 
 
 def db_migrations():
-    if not op.exists(op.join(op.dirname(op.realpath(__file__)), 'migration')):
+    if not op.exists(op.join(op.dirname(op.realpath(__file__)), 'migrations')):
         subprocess.run(['flask', 'db', 'init'])
     subprocess.run(['flask', 'db', 'migrate'])
     subprocess.run(['flask', 'db', 'upgrade'])

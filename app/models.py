7bc7ddec4eeb4f39a107304d7559ba9eb9bd6c93
Karl <karl@ltek.me>
2024-04-17 05:32:29 +0000

    refacor threaded google cloud.
    added repr for gallerycategory

--- app/models.py
+++ app/models.py
@@ -167,6 +167,9 @@ class GalleryCategory(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False, unique=True)
 
+    def __repr__(self) -> str:
+        return f'<GalleryPost {self.id}:{self.name}>'
+
 
 class GalleryPost(db.Model):
     __tablename__ = 'gallerypost'

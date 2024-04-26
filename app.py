f9ed64887a3e6afeef5c1ae8b2434dedba64a653
Karl <karl@ltek.me>
2024-04-26 15:51:30 +0000

    complete docker deployment

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from dotenv import load_dotenv
 load_dotenv('.flaskenv')
 
-from app import flask_app
+from app import flask_app, db, models
 
 from os import path as op
 import subprocess
@@ -15,6 +15,20 @@ if __name__ == "__main__":
     subprocess.run(['flask', 'db', 'migrate'])
     subprocess.run(['flask', 'db', 'upgrade'])
 
+    flask_app.app_context().push()
+    
+    # if no admin, create one
+    admins = models.UserRole.query.filter_by(name='admin').all()
+    if not admins:
+        admin = models.User(
+            display_name = 'Admin',
+            username = 'admin',
+            email = 'admin@local',
+            role_name= 'admin')
+        admin.set_password('admin')
+        db.session.add(admin)
+        db.session.commit()
+        
     flask_app.run(
         host='0.0.0.0',
         port=5000,
